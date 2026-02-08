a82f2d65b90fdf04e2861b29be851ca2d3361fc1
Mubin <fathummubin26@gmail.com>
2026-02-09 00:43:18 +0600

    Update backend and frontend files

--- Simple Problems/prime_check.cpp
+++ Simple Problems/prime_check.cpp
@@ -12,7 +12,7 @@ int main() {
     if (n == 0 || n == 1) {
         isPrime = false;
     } else {
-        fo2; i <= n / 2; ++i) {
+        for (i = 2; i <= n / 2; ++i) {
             if (n % i == 0) {
                 isPrime = false;
                 break;
